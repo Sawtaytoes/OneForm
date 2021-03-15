9ed58fe24d911e9858edd921b2c4986f9e0d384b
Kevin Ghadyani <Saturn2888@gmail.com>
2021-03-14 23:21:00 -0500

    ✨ Added FieldErrorMessage component

---
+++ packages/react/src/FieldErrorMessage.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+import {
+	memo,
+} from 'react'
+
+import useFieldErrorMessages from './useFieldErrorMessages'
+
+const propTypes = {
+	name: PropTypes.string.isRequired,
+}
+
+const FieldErrorMessage = ({
+	name,
+}) => {
+	const {
+		errorMessages = [
+			'',
+		],
+	} = (
+		useFieldErrorMessages({
+			name,
+		})
+	)
+
+	return (
+		errorMessages
+		[0]
+	)
+}
+
+FieldErrorMessage.propTypes = propTypes
+
+const MemoizedFieldErrorMessage = memo(FieldErrorMessage)
+
+export default MemoizedFieldErrorMessage
---
+++ packages/react/src/FieldErrorMessage.test.js
@@ -0,0 +1,46 @@
+import {
+	render,
+	screen,
+} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import FieldErrorMessage from './FieldErrorMessage.jsx'
+import OneForm from './OneForm.jsx'
+
+describe(
+	'FieldErrorMessage',
+	() => {
+		test(
+			'contains a single error message',
+			() => {
+				const errorMessage = (
+					'You forgot the `@` sign!'
+				)
+
+				const fieldErrorMessages = {
+					email: [
+						errorMessage,
+					],
+				}
+
+				render(
+					<OneForm
+						errorMessagesState={fieldErrorMessages}
+					>
+						<FieldErrorMessage
+							name="email"
+						/>
+					</OneForm>
+				)
+
+				expect(
+					screen
+					.getByText(
+						errorMessage
+					)
+				)
+				.toBeTruthy()
+			},
+		)
+	}
+)
