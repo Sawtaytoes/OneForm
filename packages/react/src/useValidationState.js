86c5b47f1b2f100a3ac67ae1d090a29648c4cd47
Kevin Ghadyani <Saturn2888@gmail.com>
2021-03-22 01:10:33 -0500

    ⚡️ Changed .includes to .startsWith in useValidationState

--- packages/react/src/useValidationState.js
+++ packages/react/src/useValidationState.js
@@ -397,7 +397,7 @@ const useValidationState = (
 									) => (
 										(
 											fieldName
-											.includes(
+											.startsWith(
 												validationName
 											)
 										)
