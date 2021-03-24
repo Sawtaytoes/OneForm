cf596699b8f0b9b8b0548d0c8d619295fa18a54b
Kevin Ghadyani <Saturn2888@gmail.com>
2021-03-24 05:36:18 -0500

    ✅ Added missing test for not having an error message when one isn't defined

--- packages/react/src/FieldErrorMessage.test.js
+++ packages/react/src/FieldErrorMessage.test.js
@@ -11,6 +11,28 @@ describe(
 	'FieldErrorMessage',
 	() => {
 		test(
+			'renders no errors when none provided',
+			() => {
+				render(
+					<OneForm>
+						<FieldErrorMessage
+							name="email"
+						/>
+					</OneForm>
+				)
+
+				expect(
+					screen
+					.getByRole(
+						'form'
+					)
+					.innerHTML
+				)
+				.toContain('')
+			},
+		)
+
+		test(
 			'contains a single error message',
 			() => {
 				const errorMessage = (
