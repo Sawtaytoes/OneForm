7c6effa55cf42d1248c4259c2b5c6226d20d267b
Kevin Ghadyani <Saturn2888@gmail.com>
2021-03-27 22:15:43 -0500

    ✨ Added useErrorMessagesState

---
+++ packages/react/src/useErrorMessagesState.js
@@ -0,0 +1,290 @@
+import {
+	useCallback,
+	useEffect,
+	useRef,
+} from 'react'
+
+import useOnlyObservableState from './useOnlyObservableState.js'
+
+const externalErrorMessagesSymbol = Symbol()
+const initialErrorMessages = {}
+const initialLocalErrorMessages = {}
+const initialLocalErrorMessagesList = []
+
+const useErrorMessagesState = (
+	{
+		errorMessages = (
+			initialErrorMessages
+		),
+		onErrorMessages = (
+			Function
+			.prototype
+		),
+		updatedErrorMessages = (
+			initialErrorMessages
+		),
+	} = {}
+) => {
+	const onErrorMessagesRef = (
+		useRef()
+	)
+
+	onErrorMessagesRef
+	.current = (
+		onErrorMessages
+	)
+
+	const {
+		publishUndefinedValues,
+		publishValue,
+		subscribeToValue,
+	} = (
+		useOnlyObservableState()
+	)
+
+	const localErrorMessagesRef = (
+		useRef(
+			initialLocalErrorMessages
+		)
+	)
+
+	const localErrorMessagesListRef = (
+		useRef(
+			initialLocalErrorMessagesList
+		)
+	)
+
+	const getAllLocalErrorMessages = (
+		useCallback(
+			() => (
+				localErrorMessagesRef
+				.current
+			),
+			[],
+		)
+	)
+
+	const getLocalErrorMessages = (
+		useCallback(
+			(
+				identifier,
+			) => (
+				getAllLocalErrorMessages()
+				[identifier]
+			),
+			[
+				getAllLocalErrorMessages,
+			],
+		)
+	)
+
+	const getAllLocalErrorMessagesList = (
+		useCallback(
+			() => (
+				localErrorMessagesListRef
+				.current
+			),
+			[],
+		)
+	)
+
+	const getLocalErrorMessagesList = (
+		useCallback(
+			(
+				identifier,
+			) => (
+				Array
+				.from(
+					(
+						(
+							getLocalErrorMessages(
+								identifier
+							)
+						)
+						|| new Map()
+					)
+					.values()
+				)
+				.flat()
+			),
+			[
+				getLocalErrorMessages,
+			],
+		)
+	)
+
+	const cacheAllLocalErrorMessagesList = (
+		useCallback(
+			() => {
+				localErrorMessagesListRef
+				.current = (
+					Object
+					.values(
+						localErrorMessagesRef
+						.current
+					)
+					.map((
+						errorsMap,
+					) => (
+						Array
+						.from(
+							errorsMap
+							.values()
+						)
+					))
+					.flat()
+					.flat()
+				)
+			},
+			[],
+		)
+	)
+
+	const setLocalErrorMessages = (
+		useCallback(
+			(
+				identifier,
+				{
+					errorMessages: nextErrorMessages,
+					symbol,
+				},
+			) => {
+				localErrorMessagesRef
+				.current = {
+					...(
+						getAllLocalErrorMessages()
+					),
+					[identifier]: (
+						new Map(
+							getLocalErrorMessages(
+								identifier
+							)
+						)
+						.set(
+							symbol,
+							nextErrorMessages,
+						)
+					),
+				}
+
+				const errorMessageslist = (
+					getLocalErrorMessagesList(
+						identifier
+					)
+				)
+
+				if (
+					(
+						errorMessageslist
+						.length
+					)
+					=== 0
+				) {
+					localErrorMessagesRef
+					.current = {
+						...(
+							localErrorMessagesRef
+							.current
+						),
+					}
+
+					Reflect
+					.deleteProperty(
+						(
+							localErrorMessagesRef
+							.current
+						),
+						identifier,
+					)
+				}
+
+				cacheAllLocalErrorMessagesList()
+
+				onErrorMessagesRef
+				.current({
+					errorMessages: errorMessageslist,
+					identifier,
+				})
+
+				publishValue(
+					identifier,
+					errorMessageslist,
+				)
+			},
+			[
+				cacheAllLocalErrorMessagesList,
+				getAllLocalErrorMessages,
+				getLocalErrorMessages,
+				getLocalErrorMessagesList,
+				publishValue,
+			],
+		)
+	)
+
+	useEffect(
+		() => {
+			localErrorMessagesRef
+			.current = (
+				initialLocalErrorMessages
+			)
+
+			publishUndefinedValues()
+
+			Object
+			.entries(
+				errorMessages
+			)
+			.forEach(([
+				identifier,
+				errorMessages,
+			]) => {
+				setLocalErrorMessages(
+					identifier,
+					{
+						errorMessages,
+						symbol: externalErrorMessagesSymbol,
+					},
+				)
+			})
+		},
+		[
+			errorMessages,
+			publishUndefinedValues,
+			setLocalErrorMessages,
+		],
+	)
+
+	useEffect(
+		() => {
+			Object
+			.entries(
+				updatedErrorMessages
+			)
+			.forEach(([
+				identifier,
+				errorMessages,
+			]) => {
+				setLocalErrorMessages(
+					identifier,
+					{
+						errorMessages,
+						symbol: externalErrorMessagesSymbol,
+					},
+				)
+			})
+		},
+		[
+			setLocalErrorMessages,
+			updatedErrorMessages,
+		],
+	)
+
+	return {
+		getAllErrorMessages: getAllLocalErrorMessagesList,
+		getErrorMessages: getLocalErrorMessagesList,
+		setErrorMessages: setLocalErrorMessages,
+		subscribeToErrorMessages: subscribeToValue,
+	}
+}
+
+export default useErrorMessagesState
---
+++ packages/react/src/useErrorMessagesState.test.js
@@ -0,0 +1,642 @@
+import {
+	act,
+	renderHook,
+} from '@testing-library/react-hooks'
+
+import useErrorMessagesState from './useErrorMessagesState.js'
+
+describe(
+	'useErrorMessagesState',
+	() => {
+		test(
+			'have no value when none set',
+			() => {
+				const {
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+					)
+				)
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'name',
+					)
+				)
+				.toEqual(
+					[]
+				)
+			}
+		)
+
+		test(
+			'sets error messages',
+			() => {
+				const error = {
+					errorMessages: [
+						'You must enter a value.',
+						'You forgot an `@` sign.',
+					],
+					symbol: Symbol(),
+				}
+
+				const identifier = 'email'
+
+				const {
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+					)
+				)
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						identifier,
+						error,
+					)
+				})
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						identifier
+					)
+				)
+				.toEqual(
+					error
+					.errorMessages
+				)
+
+				expect(
+					result
+					.current
+					.getAllErrorMessages()
+				)
+				.toEqual(
+					error
+					.errorMessages
+				)
+			}
+		)
+
+		test(
+			'replaces error messages based on a symbol',
+			() => {
+				const symbol1 = Symbol()
+				const symbol2 = Symbol()
+
+				const identifier = 'email'
+
+				const {
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+					)
+				)
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						identifier,
+						{
+							errorMessages: [
+								'You must enter a value.',
+								'You forgot an `@` sign.',
+							],
+							symbol: symbol1,
+						},
+					)
+				})
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						identifier,
+						{
+							errorMessages: [
+								'Your email is invalid.',
+							],
+							symbol: symbol2,
+						},
+					)
+				})
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						identifier,
+						{
+							errorMessages: [
+								'You must enter a value.',
+							],
+							symbol: symbol1,
+						},
+					)
+				})
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						identifier
+					)
+				)
+				.toEqual([
+					'You must enter a value.',
+					'Your email is invalid.',
+				])
+
+				expect(
+					result
+					.current
+					.getAllErrorMessages()
+				)
+				.toEqual([
+					'You must enter a value.',
+					'Your email is invalid.',
+				])
+			}
+		)
+
+		test(
+			'publish error messages to subscribers',
+			() => {
+				const {
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+					)
+				)
+
+				const subscriber = (
+					jest
+					.fn()
+				)
+
+				const unsubscribe1Ref = {
+					current: null,
+				}
+
+				const unsubscribe2Ref = {
+					current: null,
+				}
+
+				act(() => {
+					unsubscribe1Ref
+					.current = (
+						result
+						.current
+						.subscribeToErrorMessages({
+							identifier: 'name',
+							subscriber,
+						})
+					)
+				})
+
+				act(() => {
+					unsubscribe2Ref
+					.current = (
+						result
+						.current
+						.subscribeToErrorMessages({
+							identifier: 'name',
+							subscriber,
+						})
+					)
+				})
+
+				const errorMessages = [
+					'You need to enter a name.',
+				]
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						'name',
+						{
+							errorMessages,
+							symbol: Symbol(),
+						},
+					)
+				})
+
+				unsubscribe1Ref
+				.current()
+
+				unsubscribe2Ref
+				.current()
+
+				expect(
+					subscriber
+				)
+				.toHaveBeenCalledTimes(
+					2
+				)
+
+				expect(
+					subscriber
+				)
+				.toHaveBeenNthCalledWith(
+					1,
+					errorMessages,
+				)
+
+				expect(
+					subscriber
+				)
+				.toHaveBeenNthCalledWith(
+					2,
+					errorMessages,
+				)
+			}
+		)
+
+		test(
+			'notifies when error messages occur',
+			() => {
+				const errorMessagesCallback = (
+					jest
+					.fn()
+				)
+
+				const {
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+						{
+							initialProps: {
+								onErrorMessages: (
+									errorMessagesCallback
+								),
+							},
+						}
+					)
+				)
+
+				const identifier = 'email'
+
+				const errorMessages1 = [
+					'You need to enter a name.',
+				]
+
+				const error1 = {
+					errorMessages: errorMessages1,
+					symbol: Symbol(),
+				}
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						identifier,
+						error1,
+					)
+				})
+
+				const errorMessages2 = [
+					'Your name is too long.',
+				]
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						identifier,
+						{
+							errorMessages: errorMessages2,
+							symbol: Symbol(),
+						},
+					)
+				})
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						identifier,
+						error1,
+					)
+				})
+
+				expect(
+					errorMessagesCallback
+				)
+				.toHaveBeenCalledTimes(
+					3
+				)
+
+				expect(
+					errorMessagesCallback
+				)
+				.toHaveBeenNthCalledWith(
+					1,
+					{
+						errorMessages: errorMessages1,
+						identifier,
+					},
+				)
+
+				expect(
+					errorMessagesCallback
+				)
+				.toHaveBeenNthCalledWith(
+					2,
+					{
+						errorMessages: [
+							...errorMessages1,
+							...errorMessages2,
+						],
+						identifier,
+					},
+				)
+
+				expect(
+					errorMessagesCallback
+				)
+				.toHaveBeenNthCalledWith(
+					3,
+					{
+						errorMessages: [
+							...errorMessages1,
+							...errorMessages2,
+						],
+						identifier,
+					},
+				)
+			}
+		)
+
+		test(
+			'initializes with merged error messages',
+			() => {
+				const emailErrorMessages = [
+					'You must enter an email.',
+					'You forgot the `@` sign.',
+				]
+
+				const nameErrorMessages = [
+					'You must enter a name.',
+				]
+
+				const {
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+						{
+							initialProps: {
+								updatedErrorMessages: {
+									email: emailErrorMessages,
+									name: nameErrorMessages,
+								},
+							},
+						},
+					)
+				)
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'email',
+					)
+				)
+				.toEqual(
+					emailErrorMessages
+				)
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'name',
+					)
+				)
+				.toEqual(
+					nameErrorMessages
+				)
+			}
+		)
+
+		test(
+			'merges error messages when error messages updates',
+			() => {
+				const {
+					rerender,
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+					)
+				)
+
+				const nameErrorMessages1 = [
+					'You must enter a name.',
+				]
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						'name',
+						{
+							errorMessages: nameErrorMessages1,
+							symbol: Symbol(),
+						},
+					)
+				})
+
+				const emailErrorMessages = [
+					'You must enter an email.',
+					'You forgot the `@` sign.',
+				]
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						'email',
+						{
+							errorMessages: emailErrorMessages,
+							symbol: Symbol(),
+						},
+					)
+				})
+
+				const nameErrorMessages2 = [
+					'Your name is too long.',
+				]
+
+				rerender({
+					updatedErrorMessages: {
+						name: nameErrorMessages2,
+					},
+				})
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'email',
+					)
+				)
+				.toEqual(
+					emailErrorMessages
+				)
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'name',
+					)
+				)
+				.toEqual([
+					...nameErrorMessages1,
+					...nameErrorMessages2,
+				])
+			}
+		)
+
+		test(
+			'initializes with overwritten error messages',
+			() => {
+				const emailErrorMessages = [
+					'You must enter an email.',
+					'You forgot the `@` sign.',
+				]
+
+				const nameErrorMessages = [
+					'You must enter a name.',
+				]
+
+				const {
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+						{
+							initialProps: {
+								errorMessages: {
+									email: emailErrorMessages,
+									name: nameErrorMessages,
+								},
+							},
+						},
+					)
+				)
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'email',
+					)
+				)
+				.toEqual(
+					emailErrorMessages
+				)
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'name',
+					)
+				)
+				.toEqual(
+					nameErrorMessages
+				)
+			}
+		)
+
+		test(
+			'overwrites all error messages when given new error messages',
+			() => {
+				const {
+					rerender,
+					result,
+				} = (
+					renderHook(
+						useErrorMessagesState,
+					)
+				)
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						'name',
+						{
+							errorMessages: [
+								'You must enter a name.',
+							],
+							symbol: Symbol(),
+						},
+					)
+				})
+
+				act(() => {
+					result
+					.current
+					.setErrorMessages(
+						'email',
+						{
+							errorMessages: [
+								'You must enter an email.',
+								'You forgot the `@` sign.',
+							],
+							symbol: Symbol(),
+						},
+					)
+				})
+
+				const nameErrorMessages = [
+					'Your name is too long.',
+				]
+
+				rerender({
+					errorMessages: {
+						name: nameErrorMessages,
+					},
+				})
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'email',
+					)
+				)
+				.toEqual(
+					[]
+				)
+
+				expect(
+					result
+					.current
+					.getErrorMessages(
+						'name',
+					)
+				)
+				.toEqual(
+					nameErrorMessages
+				)
+			}
+		)
+	}
+)
