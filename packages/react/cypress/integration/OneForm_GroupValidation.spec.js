908a9a5dd3aef86f10fdfb20a8a97f266f1ca28d
Kevin Ghadyani <Saturn2888@gmail.com>
2021-03-29 01:12:16 -0500

    ✅ Added OneForm_GroupValidation Cypress test

---
+++ packages/react/cypress/integration/OneForm_GroupValidation.spec.js
@@ -0,0 +1,66 @@
+describe(
+	'OneForm GroupValidation',
+	() => {
+		it(
+			'Validates a single field.',
+			() => {
+				cy
+				.visit(
+					'oneform--group-validation'
+				)
+
+				cy
+				.findByPlaceholderText(
+					'Message 1'
+				)
+				.type(
+					'aa'
+				)
+				.should(
+					'have.value',
+					'aa'
+				)
+
+				cy
+				.findByRole(
+					'button'
+				)
+				.click()
+
+				cy
+				.findByText(
+					'Messages need to be identical.'
+				)
+				.should(
+					'to.exist'
+				)
+
+				cy
+				.findByPlaceholderText(
+					'Message 2'
+				)
+				.type(
+					'aa'
+				)
+				.should(
+					'have.value',
+					'aa'
+				)
+
+				cy
+				.findByText(
+					'Messages need to be identical.'
+				)
+				.should(
+					'to.not.exist'
+				)
+
+				cy
+				.findByRole(
+					'button'
+				)
+				.click()
+			},
+		)
+	}
+)
