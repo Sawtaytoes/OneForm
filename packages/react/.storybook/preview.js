e4cee17771c172ef3c3c7c52704e3305355ea2a0
Kevin Ghadyani <Saturn2888@gmail.com>
2021-04-04 22:01:14 -0500

    💄 Added basic CSS styling to react/ Storybook

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,3 +1,33 @@
+import { Fragment } from 'react'
+
+export const decorators = [
+	(
+		Story,
+	) => (
+		<Fragment>
+			<style>
+				{
+					`
+						*,
+						*::before,
+						*::after {
+							box-sizing: border-box;
+							font-family: sans-serif;
+							font-size: 24px;
+						}
+
+						input {
+							padding: 10px;
+						}
+					`
+				}
+			</style>
+
+			<Story />
+		</Fragment>
+	)
+]
+
 export const parameters = {
 	actions: {
 		argTypesRegex: '^on[A-Z].*',
