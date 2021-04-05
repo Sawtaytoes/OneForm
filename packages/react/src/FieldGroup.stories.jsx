e77d2191b9aa0dc323e440933504c08253980b56
Kevin Ghadyani <Saturn2888@gmail.com>
2021-04-04 22:27:09 -0500

    ♻️ Converted FieldGroup stories from MDX to JSX

---
+++ packages/react/src/FieldGroup.stories.jsx
@@ -0,0 +1,48 @@
+import Field from './Field.jsx'
+import FieldGroup from './FieldGroup.jsx'
+import OneForm from './OneForm.jsx'
+
+export default {
+	title: 'Components/FieldGroup',
+}
+
+export const Sample = () => (
+	<OneForm>
+		<FieldGroup
+			id="1"
+			name="addressId"
+		>
+			<div>
+				<Field>
+					<input
+						name="name"
+					/>
+				</Field>
+			</div>
+			<FieldGroup
+				id="1"
+				name="emailId"
+			>
+				<div>
+					<Field>
+						<input
+							name="email"
+						/>
+					</Field>
+				</div>
+			</FieldGroup>
+			<FieldGroup
+				id="2"
+				name="emailId"
+			>
+				<div>
+					<Field>
+						<input
+							name="email"
+						/>
+					</Field>
+				</div>
+			</FieldGroup>
+		</FieldGroup>
+	</OneForm>
+)
