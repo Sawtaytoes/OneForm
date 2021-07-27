a896fe2f659408d22e2f03347db0624c7cd4edc2
Kevin Ghadyani <Saturn2888@gmail.com>
2021-07-27 04:13:37 -0500

    ✨ Added `IfFieldVisitation`

---
+++ packages/react/src/IfFieldVisitation.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+import {
+  memo,
+} from 'react'
+
+import useFieldVisitation from './useFieldVisitation'
+
+const propTypes = {
+  children: PropTypes.node,
+  fallback: PropTypes.node,
+  getIsVisible: PropTypes.func,
+  name: PropTypes.string.isRequired,
+}
+
+const IfFieldVisitation = ({
+  children,
+  fallback = null,
+  name,
+}) => {
+  const {
+    isVisited = false,
+  } = (
+    useFieldVisitation({
+      name,
+    })
+  )
+
+  return (
+    isVisited
+    ? children
+    : fallback
+  )
+}
+
+IfFieldVisitation.propTypes = propTypes
+
+const MemoizedIfFieldVisitation = memo(IfFieldVisitation)
+
+export default MemoizedIfFieldVisitation
---
+++ packages/react/src/IfFieldVisitation.test.js
@@ -0,0 +1,150 @@
+import {
+  render,
+  screen,
+} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import IfFieldVisitation from './IfFieldVisitation.jsx'
+import OneForm from './OneForm.jsx'
+
+describe(
+  'IfFieldVisitation',
+  () => {
+    test(
+      'hides children when field not visited.',
+      () => {
+        const children = 'Hello World!'
+
+        render(
+          <OneForm>
+            <IfFieldVisitation
+              name="email"
+            >
+              {children}
+            </IfFieldVisitation>
+          </OneForm>
+        )
+
+        expect(
+          screen
+          .getByRole(
+            'form'
+          )
+          .innerHTML
+        )
+        .toBe(
+          ''
+        )
+      },
+    )
+
+    test(
+      'shows a children when visited.',
+      () => {
+        const children = 'Hello World!'
+
+        render(
+          <OneForm
+            values={{
+              email: 'john.smith@test.com',
+            }}
+          >
+            <IfFieldVisitation
+              name="email"
+            >
+              {children}
+            </IfFieldVisitation>
+          </OneForm>
+        )
+
+        expect(
+          screen
+          .getByText(
+            'Hello World!'
+          )
+        )
+        .toBeTruthy()
+      },
+    )
+
+    test(
+      'shows the fallback when not visited.',
+      () => {
+        const children = 'Hello World!'
+        const fallbackValue = 'Goodbye World!'
+
+        render(
+          <OneForm>
+            <IfFieldVisitation
+              fallback={fallbackValue}
+              name="email"
+            >
+              {children}
+            </IfFieldVisitation>
+          </OneForm>
+        )
+
+        expect(
+          screen
+          .getByText(
+            fallbackValue
+          )
+        )
+        .toBeTruthy()
+
+        expect(
+          screen
+          .getByRole(
+            'form'
+          )
+          .innerHTML
+        )
+        .toBe(
+          fallbackValue
+        )
+      },
+    )
+
+    test(
+      'shows only children and not the fallback.',
+      () => {
+        const children = 'Hello World!'
+        const fallbackValue = 'Goodbye World!'
+
+        render(
+          <OneForm
+            values={{
+              email: 'john.smith@test.com',
+            }}
+          >
+            <IfFieldVisitation
+              fallback={fallbackValue}
+              name="email"
+            >
+              {children}
+            </IfFieldVisitation>
+          </OneForm>
+        )
+
+        expect(
+          screen
+          .getByText(
+            children
+          )
+        )
+        .toBeTruthy()
+
+        expect(
+          screen
+          .getByRole(
+            'form'
+          )
+          .innerHTML
+        )
+        .toBe(
+          children
+        )
+      },
+    )
+  }
+)
