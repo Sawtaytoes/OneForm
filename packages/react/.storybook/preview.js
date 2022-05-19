25fdfa49e740881a824eb95619c7f899e8026565
Kevin Ghadyani <Saturn2888@gmail.com>
2022-05-19 05:14:34 -0500

    🔧 Updated Storybook config

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -2,4 +2,10 @@ export const parameters = {
   actions: {
     argTypesRegex: '^on[A-Z].*',
   },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
 }
