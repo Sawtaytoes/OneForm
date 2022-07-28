df2ba32d0814767432dcec9e953e02aef2da97e6
Kevin Ghadyani <Saturn2888@gmail.com>
2022-07-27 20:56:47 -0500

    🔥 Removed Solid project for now

--- packages/solid/cypress/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-const cypressEslintConfig = {
-  rules: {
-    'jest/expect-expect': 'off',
-    'jest/valid-expect': 'off',
-  },
-}
-
-module.exports = cypressEslintConfig
--- packages/solid/src/ValuesContext.js
+++
@@ -1,9 +0,0 @@
-import {
-  createContext,
-} from 'solid-js'
-
-const ValuesContext = (
-  createContext({})
-)
-
-export default ValuesContext
