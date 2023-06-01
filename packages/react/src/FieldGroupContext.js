1e904120970f246e0cbf7a7c26efedc604fd2b2f
Giselle Ghadyani <giselleghadyani@gmail.com>
2023-05-31 21:52:58 -0500

    🔥 Removed generated js file

--- packages/react/src/FieldGroupContext.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.FieldGroupContext = exports.defaultFieldGroupContextValue = void 0;
-var react_1 = require("react");
-exports.defaultFieldGroupContextValue = {
-    fieldGroups: []
-};
-exports.FieldGroupContext = ((0, react_1.createContext)(exports.defaultFieldGroupContextValue));
