f2d07240f22b9c439be9a9a28341ebb75112f275
Giselle Ghadyani <giselleghadyani@gmail.com>
2023-05-31 23:24:55 -0500

    🚨 Fixed type errors for useSubformData

--- packages/react/src/useSubformData.ts
+++ packages/react/src/useSubformData.ts
@@ -8,6 +8,11 @@ import {
   SubformContextType,
 } from './SubformContext'
 
+type SubformWrapperType = {
+  func: (...args: any) => void,
+  subformId: symbol,
+}
+
 export const useSubformData = <
   ValueType
 >() => {
@@ -30,57 +35,139 @@ export const useSubformData = <
     )
   )
 
-  const segmentedSubformContext = (
-    useMemo(
-      () => (
-        Object
-        .fromEntries(
-          Object
-          .entries(
-            subformContext
-          )
-          .map(([
-            functionName,
-            func,
-          ]) => ([
-            functionName,
-            (
-              (
-                func
-                .length
-              )
-              === 2
-            )
-            ? (
-              (
-                value: (
-                  Parameters<
-                    typeof func
-                  >[1]
-                ),
-              ) => (
-                func(
-                  subformId,
-                  value,
-                )
-              )
-            )
-            : (
-              () => (
-                func(
-                  subformId,
-                )
-              )
-            ),
-          ]))
-        )
-      ),
-      [
-        subformContext,
+  const addSubformIdToRemoveFunction = ({
+    func,
+    subformId,
+  }: (
+    SubformWrapperType
+  )) => (
+    () => (
+      func(
         subformId,
-      ],
+      )
     )
   )
 
-  return segmentedSubformContext
+  const addSubformIdToAddFunction = ({
+    func,
+    subformId,
+  }: (
+    SubformWrapperType
+  )) => (
+    (
+      value: (
+        Parameters<
+          typeof func
+        >[1]
+      ),
+    ) => (
+      func(
+        subformId,
+        value,
+      )
+    )
+  )
+
+  return {
+    addErrorMessages: (
+      addSubformIdToAddFunction({
+        func: subformContext.addErrorMessages,
+        subformId,
+      })
+    ),
+    addGroupValidations: (
+      addSubformIdToAddFunction({
+        func: subformContext.addGroupValidations,
+        subformId,
+      })
+    ),
+    addOnChange: (
+      addSubformIdToAddFunction({
+        func: subformContext.addOnChange,
+        subformId,
+      })
+    ),
+    addOnSubmit: (
+      addSubformIdToAddFunction({
+        func: subformContext.addOnSubmit,
+        subformId,
+      })
+    ),
+    addUpdatedErrorMessages: (
+      addSubformIdToAddFunction({
+        func: subformContext.addUpdatedErrorMessages,
+        subformId,
+      })
+    ),
+    addUpdatedValues: (
+      addSubformIdToAddFunction({
+        func: subformContext.addUpdatedValues,
+        subformId,
+      })
+    ),
+    addValidations: (
+      addSubformIdToAddFunction({
+        func: subformContext.addValidations,
+        subformId,
+      })
+    ),
+    addValues: (
+      addSubformIdToAddFunction({
+        func: subformContext.addValues,
+        subformId,
+      })
+    ),
+    removeErrorMessages: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeErrorMessages,
+        subformId,
+      })
+    ),
+    removeGroupValidations: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeGroupValidations,
+        subformId,
+      })
+    ),
+    removeOnChange: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeOnChange,
+        subformId,
+      })
+    ),
+    removeOnSubmit: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeOnSubmit,
+        subformId,
+      })
+    ),
+    removeUpdatedErrorMessages: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeUpdatedErrorMessages,
+        subformId,
+      })
+    ),
+    removeUpdatedValues: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeUpdatedValues,
+        subformId,
+      })
+    ),
+    removeValidations: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeValidations,
+        subformId,
+      })
+    ),
+    removeValues: (
+      addSubformIdToRemoveFunction({
+        func: subformContext.removeValues,
+        subformId,
+      })
+    ),
+  } satisfies (
+    SubformContextType<
+      ValueType
+    >
+  )
 }
